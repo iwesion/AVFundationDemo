959b3e2ebe7bd72e485d84151c9fa01ed23ec32c
wesion <497905861@qq.com>
2021-10-12 14:28:59 +0800

    no message

--- AVFundationDemo/录制capture/人脸识别/FaceRecognitionViewController.swift
+++ AVFundationDemo/录制capture/人脸识别/FaceRecognitionViewController.swift
@@ -67,68 +67,104 @@ class FaceRecognitionViewController: UIViewController {
     }
     
     func showFace(_ metadataObjects:[AVMetadataObject]) {
-//        //获取faceLayers的key，用于确定哪些人移除了视图并将对应的图层移出界面。
-//        /*
-//         支持同时识别10个人脸
-//         */
-//        var lostFaces:[Int] = []
-//        for key in self.faceLayers.keys {
-//            lostFaces.append(key)
-//        }
-//        for metadataObject in metadataObjects {
-//            let face = metadataObject as! AVMetadataFaceObject
-//            let faceID:Int = face.faceID
-//            //将对象从lostFaces 移除,留下的就是不要的（到时候要删除）
-//            print("删除前\(lostFaces),\(faceID)")
-//            lostFaces =  lostFaces.filter { i in
-//                return i != faceID
-//            }
-//            print("删除后\(lostFaces),\(faceID)")
-//            var layer:CALayer? = self.faceLayers[faceID]
-//            //拿到当前faceID对应的layer
-//            if (layer == nil) {
-//                //调用makeFaceLayer 创建一个新的人脸图层
-//                layer = self.makeFaceLayer()
-//                //将新的人脸图层添加到 previewLayer上
-//                self.overlayLayer.addSublayer(layer!);
-//                //将layer加入到字典中
-//                self.faceLayers[faceID] = layer;
-//            }
-//            //设置图层的transform属性 CATransform3DIdentity 图层默认变化 这样可以重新设置之前应用的变化
-//            layer!.transform = CATransform3DIdentity
-//            //图层的大小 = 人脸的大小
-//            DispatchQueue.main.async { [weak self] in
-//                layer!.frame = face.bounds;
-//            }
-//
-//        }
-//        //遍历数组将剩下的人脸ID集合从上一个图层和faceLayers字典中移除
-//        for faceId in lostFaces {
-//            let layer:CALayer = faceLayers[faceId]!
-//            layer.removeFromSuperlayer()
-//            faceLayers.removeValue(forKey: faceId)
-//        }
-//
-//        self.overlayLayer.layoutIfNeeded()
-        
-        
-        faceLayerArr.removeAll()
-        overlayLayer.sublayers?.removeAll()
-        faceLayers.removeAll()
+        //获取faceLayers的key，用于确定哪些人移除了视图并将对应的图层移出界面。
+        /*
+         支持同时识别10个人脸
+         */
+        var lostFaces:[Int] = []
+        for key in self.faceLayers.keys {
+            lostFaces.append(key)
+        }
         for metadataObject in metadataObjects {
             let face = metadataObject as! AVMetadataFaceObject
             let faceID:Int = face.faceID
-            var layer:CALayer = self.makeFaceLayer()
+            //将对象从lostFaces 移除,留下的就是不要的（到时候要删除）
             
-            DispatchQueue.main.async { [weak self] in
-                layer.frame = face.bounds;
-                self?.overlayLayer.addSublayer(layer)
-                self?.faceLayerArr.append(layer)
-                
+            lostFaces =  lostFaces.filter { i in
+                return i != faceID
             }
             
-            
+            var layer:CALayer? = self.faceLayers[faceID]
+            //拿到当前faceID对应的layer
+            if (layer == nil) {
+                //调用makeFaceLayer 创建一个新的人脸图层
+                layer = self.makeFaceLayer()
+                //将新的人脸图层添加到 previewLayer上
+                self.overlayLayer.addSublayer(layer!);
+                //将layer加入到字典中
+                self.faceLayers[faceID] = layer;
+            }
+            //设置图层的transform属性 CATransform3DIdentity 图层默认变化 这样可以重新设置之前应用的变化
+            layer!.transform = CATransform3DIdentity
+            if (face.hasRollAngle) {
+                
+                //如果为YES,则获取相应的CATransform3D 值
+                let t:CATransform3D = self.transformForRollAngle(face.rollAngle);
+                //将它与标识变化关联在一起，并设置transform属性
+                layer!.transform = CATransform3DConcat(layer!.transform, t);
+            }
+            //判断人脸对象是否具有有效的偏转角
+            if (face.hasYawAngle) {
+                
+                //如果为YES,则获取相应的CATransform3D 值
+                let t:CATransform3D = self.transformForYawAngle(face.yawAngle);
+                layer!.transform = CATransform3DConcat(layer!.transform, t);
+                
+            }
+            //图层的大小 = 人脸的大小
+            DispatchQueue.main.async { [weak self] in
+                layer!.frame = face.bounds;
+            }
+
         }
+        //遍历数组将剩下的人脸ID集合从上一个图层和faceLayers字典中移除
+        for faceId in lostFaces {
+            let layer:CALayer = faceLayers[faceId]!
+            layer.backgroundColor = UIColor.green.cgColor
+            DispatchQueue.main.async { [weak self] in
+                layer.removeFromSuperlayer()
+            }
+           
+            faceLayers.removeValue(forKey: faceId)
+            
+           
+        }
+        
+        
+//        faceLayerArr.removeAll()
+//        overlayLayer.sublayers?.removeAll()
+//        faceLayers.removeAll()
+//        for metadataObject in metadataObjects {
+//            let face = metadataObject as! AVMetadataFaceObject
+//            let faceID:Int = face.faceID
+//            var layer:CALayer = self.makeFaceLayer()
+//            //判断人脸对象是否具有有效的斜倾交。
+//            if (face.hasRollAngle) {
+//
+//                //如果为YES,则获取相应的CATransform3D 值
+//                let t:CATransform3D = self.transformForRollAngle(face.rollAngle);
+//                //将它与标识变化关联在一起，并设置transform属性
+//                layer.transform = CATransform3DConcat(layer.transform, t);
+//            }
+//            //判断人脸对象是否具有有效的偏转角
+//            if (face.hasYawAngle) {
+//
+//                //如果为YES,则获取相应的CATransform3D 值
+//                let t:CATransform3D = self.transformForYawAngle(face.yawAngle);
+//                layer.transform = CATransform3DConcat(layer.transform, t);
+//
+//            }
+//
+//
+//            DispatchQueue.main.async { [weak self] in
+//                layer.frame = face.bounds;
+//                self?.overlayLayer.addSublayer(layer)
+//                self?.faceLayerArr.append(layer)
+//
+//            }
+//
+//
+//        }
         
         
     }
@@ -151,8 +187,8 @@ class FaceRecognitionViewController: UIViewController {
     }
     func makeFaceLayer() -> CALayer {
         let layer = CALayer.init()
-        //边框宽度为1.0f
-        layer.borderWidth = 1.0
+        //边框宽度为3.0f
+        layer.borderWidth = 3.0
         //边框颜色为红色
         layer.borderColor = UIColor.red.cgColor
         if showStar {
@@ -179,5 +215,45 @@ class FaceRecognitionViewController: UIViewController {
         transform.m34 = -1/eyePosition
         return transform
         
+    }
+    //将 RollAngle 的 rollAngleInDegrees 值转换为 CATransform3D
+    func transformForRollAngle(_ rollAngleInDegrees:CGFloat) -> CATransform3D {
+        //将人脸对象得到的RollAngle 单位“度” 转为Core Animation需要的弧度值
+        let rollAngleInRadians:CGFloat = rollAngleInDegrees * Double.pi / 180
+        //将结果赋给CATransform3DMakeRotation x,y,z轴为0，0，1 得到绕Z轴倾斜角旋转转换
+        return CATransform3DMakeRotation(rollAngleInRadians, 0, 0, 1)
+        
+    }
+    //将 YawAngle 的 yawAngleInDegrees 值转换为 CATransform3D
+    func transformForYawAngle(_ yawAngleInDegrees:CGFloat) -> CATransform3D {
+        //将角度转换为弧度值
+        let yawAngleInRaians:CGFloat = yawAngleInDegrees * Double.pi / 180
+        //将结果赋给CATransform3DMakeRotation x,y,z轴为0，0，1 得到绕Z轴倾斜角旋转转换
+        let yawTransform:CATransform3D = CATransform3DMakeRotation(yawAngleInRaians, 0, -1, 0)
+        
+        
+        return CATransform3DConcat(yawTransform, self.orientationTransform())
+        
+    }
+    
+    func orientationTransform() -> CATransform3D {
+        var angle:CGFloat = 0
+        
+        switch UIDevice.current.orientation {
+        case .portraitUpsideDown:
+            angle = .pi
+            //方向：右
+        case .landscapeRight:
+            angle = -(.pi / 2.0)
+            //方向：左
+        case .landscapeLeft:
+            angle = .pi / 2.0
+            //其他
+        default:
+            angle = 0.0
+            break;
+        }
+        
+        return CATransform3DMakeRotation(angle, 0.0, 0.0, 1.0);
     }
 }
